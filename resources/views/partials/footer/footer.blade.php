677993ce6e5c13a24df7577a0eec7ccb768fbd7a
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-07-31 13:36:42 +0200

    creata sezione social footer

--- resources/views/partials/footer/footer.blade.php
+++ resources/views/partials/footer/footer.blade.php
@@ -2,5 +2,6 @@
     <section class="footer-section">
             @include('partials.footer.footerMerch')
             @include('partials.footer.footerNavLinks')
+            @include('partials.footer.footerSocial')
     </section>
 </footer>
--- resources/views/partials/footer/footerSocial.blade.php
+++ resources/views/partials/footer/footerSocial.blade.php
@@ -1,3 +1,32 @@
+<?php 
+$socials = config("socialLinksName");
+?>
+
+
 <section class="socialSection">
-    
+    <div class="bg">
+        <div class="container">
+            <div>
+                <button>
+                    SIGN-UP NOW!
+                </button>
+            </div>
+            <div>
+                <h4>
+                    FOLLOW US
+                </h4>
+                <div class="social">
+                    <ul>
+                        @foreach ($socials as $social)
+                        <li> {{$social['name']}}
+                            <a href="{{$social['link']}}">
+                                <img src="{{Vite::asset($social['src'])}}" alt="{{$social['name']}}">
+                            </a>
+                        </li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
